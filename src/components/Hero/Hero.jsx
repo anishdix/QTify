d0432ca0cd2b18e2dea4616b06d72a8d0a29f9f8
anish dix <anishdix777@gmail.com>
2024-01-24 10:17:33 +0530

    added hero img

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import styles from "./Hero.module.css"
-import {ReactComponent as HeroImg} from "../../assets/HeroImg"
+import HeroImg from "../../assets/HeroImg.png"
 
 const Hero = () => {
   return (
-    <div>
-        <div>
-            <h1></h1>
+    <div className={styles.hero}>
+        <div >
+            <h1>100 Thousonad Songs, ad-free</h1>
+            <h1>Over thousands podcast episodes</h1>
         </div>
         <div>
-            <img width={212} src={HeroImg} alt="Headphone" />
+            <img width={212}  src={HeroImg} alt="Headphone" />
         </div>
       
     </div>
