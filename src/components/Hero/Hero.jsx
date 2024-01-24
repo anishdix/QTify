cac9cb6168ee4888d77dd1e7b6948bf40901399a
anish dix <anishdix777@gmail.com>
2024-01-24 10:48:29 +0530

    implemented poppins font

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className={styles.hero}>
         <div >
-            <h1>100 Thousonad Songs, ad-free</h1>
+            <h1>100 Thousand Songs, ad-free</h1>
             <h1>Over thousands podcast episodes</h1>
         </div>
         <div>
